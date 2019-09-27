866f834a9c366dfb35db05b0dcc9f11b50232df2
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-09-26 18:18:39 -0700

    Dependencies added. Index set up.

--- react-redux-app/src/index.js
+++ react-redux-app/src/index.js
@@ -1,7 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
 import './index.css';
-import App from './App';
 
+const reducer = () => {
+    return 'hello'
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+    <App />
+    </Provider>, document.getElementById('root'));
